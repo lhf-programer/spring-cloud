451f0f2e5028db2f6c60e991adc47e21fa341b48
lvhaifeng <1179438473@qq.com>
2019-12-11 14:13:05 +0800

    v1.1 版本构建

--- cloud-config/src/main/java/com/lvhaifeng/cloud/config/ConfigServerBootstrap.java
+++ cloud-config/src/main/java/com/lvhaifeng/cloud/config/ConfigServerBootstrap.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
 
 /**
  * @description 配置中心
